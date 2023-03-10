a92e57a65408f3557d7a9f33a2948dfb7d00f5c5
yazhou.TD <dengyazhou@thinkingdata.cn>
2023-03-10 11:06:13 +0800

    wx.request 发送请求

---
+++ WXMPNetworkRequest.js
@@ -0,0 +1,21 @@
+console.log("WXMPNetworkRequest");
+
+// 1、wx.request 发送请求
+// var options = {
+//   url: "https://receiver-ta-demo.thinkingdata.cn/config?appid=af6861d085e14b5c948662e1fcdce6ef",
+//   method: "GET",
+//   header: {
+//     'content-type': 'application/json'
+//   },
+//   success: function success(res) {
+//     console.log("===>>> success");
+//     console.log(res);
+//     console.log(res.data.data);
+//   },
+//   fail: function fail(res) {
+//     console.log("===>>> fail");
+//     console.log(res);
+//   }
+// }
+
+// wx.request(options);
