f5e7aa3e30f9643846b897ca52767e0ae775ad97
yazhou.TD <dengyazhou@thinkingdata.cn>
2023-03-14 14:35:14 +0800

    1、wx.onNetworkStatusChange 网络变化的回调 2、wx.getNetworkType 获取网络类型的回调 3、wx.getSystemInfo 获取系统信息的回调

--- WXMPSystemMethod.js
+++ WXMPSystemMethod.js
@@ -1,4 +1,36 @@
 console.log("WXMPSystemMethod");
 
-// 1、wx
+// 1、wx 系统对象
 // console.log(wx);
+
+// 2、小游戏 才有的系统对象
+// console.log(GameGlobal);
+
+// 3、onNetworkStatusChange 网络变化的回调
+// wx.onNetworkStatusChange((result) => {
+//   console.log("=========>>>>> wx.onNetworkStatusChange: ");
+//   console.log(result.isConnected)//当前是否有网络链接，返回的是布尔值
+//   console.log(result.networkType)//返回的是网络类型
+// });
+
+// 4、getNetworkType 获取网络类型的回调
+// wx.getNetworkType({
+//   success: function (result) {
+//     console.log("=========>>>>> wx.getNetworkType success");
+//     console.log(result);
+//   },
+//   complete: function (result) {
+//     console.log("=========>>>>> wx.getNetworkType complete");
+//     console.log(result);
+//   }
+// })
+
+// 5、getSystemInfo 获取系统信息的回调
+// wx.getSystemInfo({
+//   success: (result) => {
+//     console.log("=========>>>>> wx.getSystemInfo success");
+//     console.log(result)
+//   },
+// })
+
+
